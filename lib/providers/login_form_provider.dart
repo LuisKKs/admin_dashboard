30739e6691bc35380e24a890568d3c367cb5e518
bryanzurdo <110845766+bryanzurdo@users.noreply.github.com>
2022-10-14 17:29:02 -0500

    loginAuthAvanze
    nestor se la come

---
+++ lib/providers/login_form_provider.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class LoginFormProvider extends ChangeNotifier {
+  GlobalKey<FormState> formKey = new GlobalKey<FormState>();
+
+  String email = '';
+  String password = '';
+
+  validateForm() {
+    if (formKey.currentState!.validate()) {
+      print('Form valid...Login');
+    } else {
+      print('Form not valid');
+    }
+  }
+}
--- lib/ui/views/login_view.dart
+++ lib/ui/views/login_view.dart
@@ -1,71 +1,99 @@
+import 'package:admin_dashboard/providers/login_form_provider.dart';
 import 'package:admin_dashboard/router/router.dart';
 import 'package:admin_dashboard/ui/buttons/custom_outlined_button.dart';
 import 'package:admin_dashboard/ui/buttons/link_text.dart';
 import 'package:admin_dashboard/ui/inputs/custom_inputs.dart';
+import 'package:email_validator/email_validator.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
 
 class LoginView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.only(top: 100),
-      padding: EdgeInsets.symmetric(horizontal: 20),
-      child: Center(
-        child: ConstrainedBox(
-          constraints: BoxConstraints(maxWidth: 370),
-          child: Form(
-              child: Column(
-            children: [
-              Text(
-                'Inicia Sesion ',
-                style: GoogleFonts.montserratAlternates(
-                    fontSize: 40,
-                    color: Colors.grey,
-                    fontWeight: FontWeight.bold),
-              ),
-              //email
-              TextFormField(
-                //validator(),
-                style: TextStyle(color: Colors.black),
-                decoration: CustomInputs.loginInputDecoration(
-                    hint: 'Ingrese su correo',
-                    label: 'Email',
-                    icon: Icons.email_outlined),
-              ),
+    return ChangeNotifierProvider(
+        create: (_) => LoginFormProvider(),
+        child: Builder(builder: (contex) {
+          final loginFormProvider =
+              Provider.of<LoginFormProvider>(contex, listen: false);
 
-              SizedBox(
-                height: 20,
-              ),
+          return Container(
+            margin: EdgeInsets.only(top: 100),
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            child: Center(
+              child: ConstrainedBox(
+                constraints: BoxConstraints(maxWidth: 370),
+                child: Form(
+                    key: loginFormProvider.formKey,
+                    child: Column(
+                      children: [
+                        Text(
+                          'Inicia Sesion ',
+                          style: GoogleFonts.montserratAlternates(
+                              fontSize: 40,
+                              color: Colors.grey,
+                              fontWeight: FontWeight.bold),
+                        ),
+                        //email
+                        TextFormField(
+                          validator: (value) {
+                            if (!EmailValidator.validate(value ?? ''))
+                              return 'Email no valido';
+                            return null;
+                          },
+                          onChanged: (value) => loginFormProvider.email = value,
+                          style: TextStyle(color: Colors.black),
+                          decoration: CustomInputs.loginInputDecoration(
+                              hint: 'Ingrese su correo',
+                              label: 'Email',
+                              icon: Icons.email_outlined),
+                        ),
 
-              //contraseña
-              TextFormField(
-                //validator(),
-                obscureText: true,
-                style: TextStyle(color: Colors.black),
-                decoration: CustomInputs.loginInputDecoration(
-                    hint: '**********',
-                    label: 'Contraseña',
-                    icon: Icons.lock_outline_rounded),
-              ),
+                        SizedBox(
+                          height: 20,
+                        ),
 
-              SizedBox(height: 20),
-              CustomOutlinedButton(
-                onPressed: () {},
-                text: 'Ingresar',
-              ),
+                        //contraseña
+                        TextFormField(
+                          onChanged: (value) =>
+                              loginFormProvider.password = value,
+                          validator: (value) {
+                            if (value == null || value.isEmpty)
+                              return 'Ingrese su contraseña';
+                            if (value.length < 8)
+                              return 'La contraseña debe ser de 8 digitos o mas';
 
-              SizedBox(height: 20),
-              LinkText(
-                text: 'Crear Nuevo Usuario',
-                onPressed: () {
-                  Navigator.pushNamed(context, Flurorouter.registerRoute);
-                },
+                            return null; //valido
+                          },
+                          obscureText: true,
+                          style: TextStyle(color: Colors.black),
+                          decoration: CustomInputs.loginInputDecoration(
+                              hint: '**********',
+                              label: 'Contraseña',
+                              icon: Icons.lock_outline_rounded),
+                        ),
+
+                        SizedBox(height: 20),
+                        CustomOutlinedButton(
+                          onPressed: () {
+                            loginFormProvider.validateForm();
+                          },
+                          text: 'Ingresar',
+                        ),
+
+                        SizedBox(height: 20),
+                        LinkText(
+                          text: 'Crear Nuevo Usuario',
+                          onPressed: () {
+                            Navigator.pushNamed(
+                                context, Flurorouter.registerRoute);
+                          },
+                        ),
+                      ],
+                    )),
               ),
-            ],
-          )),
-        ),
-      ),
-    );
+            ),
+          );
+        }));
   }
 }
