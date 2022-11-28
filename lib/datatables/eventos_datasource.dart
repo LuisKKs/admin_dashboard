b0e2ce804ba531f68d778d7221c2e571938d6ed7
bryanzurdo <110845766+bryanzurdo@users.noreply.github.com>
2022-11-28 16:33:37 -0600

    solucion en que no mostraba datos

--- lib/datatables/eventos_datasource.dart
+++ lib/datatables/eventos_datasource.dart
@@ -10,26 +10,27 @@ class EventosDatasource extends DataTableSource {
     final evento = this.eventos[index];
 
     return DataRow.byIndex(index: index, cells: [
-      //DataCell(Text(evento.shortName)),
-      DataCell(Text('Cell #1 $index:$index')),
-      DataCell(Text('Cell #2 $index:$index')),
-      DataCell(Text('Cell #3 $index:$index')),
-      DataCell(Text('Cell #4 $index:$index')),
-      DataCell(Text('Cell #5 $index:$index')),
-      DataCell(Text('Cell #6 $index:$index')),
-      DataCell(Text('Cell #7 $index:$index')),
-      DataCell(Text('Cell #8 $index:$index')),
-      DataCell(Text('Cell #9 $index:$index')),
-      DataCell(Text('Cell #10 $index:$index')),
-      DataCell(Text('Cell #11 $index:$index')),
-      DataCell(Text('Cell #12 $index:$index')),
-      DataCell(Text('Cell #13 $index:$index')),
-      DataCell(Text('Cell #14 $index:$index')),
+      DataCell(Text(evento.shortName)),
+      DataCell(Text(evento.eventName)),
+      DataCell(Text('Cell #3 index:$index')),
+      DataCell(Text('Cell #4 index:$index')),
+      //DataCell(Text(evento.dateStart)),
+      //DataCell(Text(evento.dateFinish)),
+      DataCell(Text(evento.eventHour)),
+      DataCell(Text(evento.logo)),
+      DataCell(Text(evento.organizer)),
+      DataCell(Text(evento.email)),
+      DataCell(Text(evento.phone)),
+      DataCell(Text(evento.website)),
+      DataCell(Text(evento.country)),
+      DataCell(Text(evento.stateCountry)),
+      DataCell(Text('Cell #13 index:$index')),
+      DataCell(Text('Cell #14 index:$index')),
     ]);
   }
 
   @override
-  bool get isRowCountApproximate => true;
+  bool get isRowCountApproximate => false;
 
   @override
   int get rowCount => eventos.length;
